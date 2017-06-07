3c3fae1fdf09d9fe80b5166ebf324f55a83b7c3e
crea <masataka0504@gmail.com>
2017-06-07 13:45:16 -0700

    doesn't transit if touch unfocused

---
+++ MovieSanctuary/MyButton.swift
@@ -0,0 +1,51 @@
+
+import UIKit
+
+extension MyButton {
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let point = touch.location(in: self)
+        
+        if self.bounds.contains(point) {
+            removeAllSubviews(parentView: parentViewControllerRootView()!)
+            NotificationCenter.default.post(name: Notification.Name("Toggle"), object: nil)
+        }
+        
+        else {
+            print("はずれた")
+            self.titleLabel?.textColor = UIColor(red: 0, green: 0.48, blue: 1, alpha: 1)
+            self.titleLabel?.alpha = 1
+        }
+        
+    }
+    
+    
+    func removeAllSubviews(parentView: UIView) {
+        
+        let subviews = parentView.subviews
+        
+        subviews.forEach{ $0.removeFromSuperview() }
+        
+    }
+    
+    
+    func parentViewControllerRootView() -> UIView? {
+        var parentResponder: UIResponder? = self
+        while true {
+            guard let nextResponder = parentResponder?.next else { return nil }
+            if let viewController = nextResponder as? UIViewController {
+                // return viewController.view.subviews[0] // こっちだとinsetが正しい
+                return viewController.view  // こっちだとinsetがおかしい
+            }
+            parentResponder = nextResponder
+        }
+    }
+    
+}
+
+class MyButton: UIButton {}
--- MovieSanctuary/SearchMovieViewController.swift
+++ MovieSanctuary/SearchMovieViewController.swift
@@ -7,49 +7,45 @@ extension UIScrollView {
     }
 }
 
-
-extension UIButton {
-    
-    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-                       
-            removeAllSubviews(parentView: parentViewControllerRootView()!)
-            
-            NotificationCenter.default.post(name: Notification.Name("Toggle"), object: nil)
-            
-        }
-    }
-
-    func removeAllSubviews(parentView: UIView) {
-        
-        parentView.backgroundColor = .orange
-        
-        print("おら、子は", parentView.subviews)
-        
-        let subviews = parentView.subviews
-        
-        subviews.forEach{ $0.removeFromSuperview() }
-        
-    }
-    
-    
-    func parentViewControllerRootView() -> UIView? {
-        var parentResponder: UIResponder? = self
-        while true {
-            guard let nextResponder = parentResponder?.next else { return nil }
-            if let viewController = nextResponder as? UIViewController {
-                // return viewController.view.subviews[0] // こっちだとinsetが正しい
-                return viewController.view  // こっちだとinsetがおかしい
-            }
-            parentResponder = nextResponder
-        }
-    }
-    
-}
+//
+//extension UIButton {
+//    
+//    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let touch = touches.first {
+//                       
+//            removeAllSubviews(parentView: parentViewControllerRootView()!)
+//            
+//            NotificationCenter.default.post(name: Notification.Name("Toggle"), object: nil)
+//            
+//        }
+//    }
+//
+//    func removeAllSubviews(parentView: UIView) {
+//        
+//        let subviews = parentView.subviews
+//        
+//        subviews.forEach{ $0.removeFromSuperview() }
+//        
+//    }
+//    
+//    
+//    func parentViewControllerRootView() -> UIView? {
+//        var parentResponder: UIResponder? = self
+//        while true {
+//            guard let nextResponder = parentResponder?.next else { return nil }
+//            if let viewController = nextResponder as? UIViewController {
+//                // return viewController.view.subviews[0] // こっちだとinsetが正しい
+//                return viewController.view  // こっちだとinsetがおかしい
+//            }
+//            parentResponder = nextResponder
+//        }
+//    }
+//    
+//}
 
 
 
-class SearchMovieViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+class SearchMovieViewController: UIViewController {
 
     let names = ["1", "2", "3", "4", "5"]
     
@@ -57,7 +53,7 @@ class SearchMovieViewController: UIViewController, UISearchBarDelegate, UITableV
         
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(makeTableView), name: Notification.Name("Toggle"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(buttonTapped), name: Notification.Name("Toggle"), object: nil)
         
         self.view.addSubview(makeScrollView())
         
@@ -65,7 +61,12 @@ class SearchMovieViewController: UIViewController, UISearchBarDelegate, UITableV
     
     
     func makeScrollView() -> SearchVCCategoryScrollView {
-        return SearchVCCategoryScrollView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        return SearchVCCategoryScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+    }
+    
+    
+    func buttonTapped() {
+        makeTableView()
     }
     
     
@@ -73,18 +74,42 @@ class SearchMovieViewController: UIViewController, UISearchBarDelegate, UITableV
         
         print("ほえええええきたーーーー")
         
+        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 44))
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.barStyle = .default
+        searchBar.showsCancelButton = true
+        searchBar.delegate = self
+        
+        
+        self.view.addSubview(searchBar)
+        
+        searchBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        
+        searchBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+        // tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        
+        // FIXME: - Emergency
+        searchBar.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 65).isActive = true
+        
         let tableView = UITableView(frame: self.view.frame, style: .plain)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.addSubview(tableView)
         
         tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        
         tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        
+        // tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        
+        // FIXME: - Emergency
+        tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor).isActive = true
+        
         tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
-        tableView.delegate   = self  // これらは、assSubViewしたあとに呼ばないとだめっぽい？？
-        tableView.dataSource = self  // こっちもね
+        tableView.delegate   = self
+        tableView.dataSource = self
         
         let xib = UINib(nibName: "TableViewCell", bundle: nil)
         
@@ -95,7 +120,7 @@ class SearchMovieViewController: UIViewController, UISearchBarDelegate, UITableV
 }
 
 
-extension SearchMovieViewController {
+extension SearchMovieViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.names.count
@@ -106,7 +131,7 @@ extension SearchMovieViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
         
         cell.titleLabel.text = self.names[indexPath.row]
-        cell.subtitleLabel.text = "さようなら"
+        cell.subtitleLabel.text = "Hello, World"
         
         return cell
         
@@ -126,6 +151,34 @@ extension SearchMovieViewController {
     }
     
 }
+
+
+extension SearchMovieViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+        
+        if searchBar.text?.characters.count == 0 {
+            print("owata")
+        } else {
+            print("non-non-non")
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
+        print("cancel")
+        
+        // self.view = nil  // これやるとだめ
+        
+        self.view.endEditing(true)
+        
+        self.view.addSubview(makeScrollView())
+        
+    }
+    
+}
+
 
 
 
